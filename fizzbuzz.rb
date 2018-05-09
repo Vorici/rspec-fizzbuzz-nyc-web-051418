601768379ef40eb30f8cfef57ea6bb21231ca855
Ledio Bode <l.bode515@gmail.com>
2018-05-09 20:06:15 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,16 @@
-def fizzbuzz
-  
-end
+def fizzbuzz(num)
+
+if num % 3 == 0 and num % 5 == 0
+		 "fizzbuzz"
+
+	else if num % 3 == 0
+	   "fizz"
+
+	else if num % 5 == 0
+		 "buzz"
+
+	else
+		return nil
+
+  end
+ end
