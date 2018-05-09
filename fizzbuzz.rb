3d1e26d7735ef47ede75cbfa68b4afc63c74a607
Ledio Bode <l.bode515@gmail.com>
2018-05-09 20:32:27 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,18 @@
+def fizzbuzz(num)
+  
+  if num % 3 == 0 and num % 5 == 0
+		 "FizzBuzz"
+		 
+	    else if num % 3 == 0
+	      "Fizz"
+
+    	else if num % 5 == 0
+		    "Buzz"
+
+	  else
+		    return nil
+
+   end
+  end
+ end
+end
