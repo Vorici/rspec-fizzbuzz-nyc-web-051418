df551240496798eb122893675fb8bdba51c68f70
Ledio Bode <l.bode515@gmail.com>
2018-05-09 19:58:56 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,17 @@
 def fizzbuzz(num)
 
-if num%3 == 0 and num%5 == 0:
+if num % 3 == 0 and num % 5 == 0
 		print "fizzbuzz"
 
-	else if num%3 == 0:
+	else if num % 3 == 0
 		print "fizz"
 
-	else if num%5 == 0:
+	else if num % 5 == 0
 		print "buzz"
 
 	else
 		return nil
 
   end
+ end
 end
